66342ff88fd42a91af7b33fefd472d2b19ce3480
Evaldas Ruseckas <evaldasr@lt.ibm.com>
2017-01-12 19:10:14 +0200

    preparing release

--- index.js
+++
@@ -1,3 +0,0 @@
-// export this package's api
-import TreeSelect from './src/';
-export default TreeSelect;
--- src/index.js
+++
@@ -1,5 +0,0 @@
-// export this package's api
-import TreeSelect from './Select.jsx';
-import TreeNode from './TreeNode.jsx';
-TreeSelect.TreeNode = TreeNode;
-export default TreeSelect;
