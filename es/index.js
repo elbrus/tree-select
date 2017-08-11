f5642c9975cf3497adef21a42d20785a9f3a4897
Evaldas Ruseckas <evaldasr@lt.ibm.com>
2017-08-11 12:10:17 +0300

    prepare release

---
+++ es/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _Select = require('./Select.js');
+
+var _Select2 = _interopRequireDefault(_Select);
+
+var _TreeNode = require('./TreeNode.js');
+
+var _TreeNode2 = _interopRequireDefault(_TreeNode);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+// export this package's api
+_Select2['default'].TreeNode = _TreeNode2['default'];
+exports['default'] = _Select2['default'];
--- src/TreeNode.jsx
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-
-export default class TreeNode extends React.Component {
-  static propTypes = {
-    value: PropTypes.string,
-  };
-}
